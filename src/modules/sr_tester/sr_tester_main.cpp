86baf0e20ccf3e680d81d9a256c67a97d1f43857
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2014-05-18 21:14:14 +0200

    Add adc logger

--- src/modules/sr_tester/sr_tester_main.cpp
+++ src/modules/sr_tester/sr_tester_main.cpp
@@ -2,7 +2,6 @@
  *
  *   Copyright (c) 2012, 2013 PX4 Development Team. All rights reserved.
  *   Author: Kim Lindberg Schwaner <kschw10@student.sdu.dk>
- *           Thomas Larsen <thola11@student.sdu.dk>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -37,115 +36,92 @@
  * @file sr_tester_main.cpp
  *
  * @author Kim Lindberg Schwaner <kschw10@student.sdu.dk>
- * @author Thomas Larsen <thola11@student.sdu.dk>
  */
 
-#include <cxx/cstdlib>
-#include <cxx/cstdio>
-#include <cxx/cstring>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <systemlib/systemlib.h>
-#include <systemlib/getopt_long.h>
 #include <systemlib/err.h>
+#include <nuttx/analog/adc.h>
+#include <drivers/drv_adc.h>
 #include <drivers/drv_hrt.h>
 #include <uORB/uORB.h>
-#include <uORB/topics/actuator_controls.h>
-#include <uORB/topics/vehicle_status.h>
+#include <uORB/topics/debug_key_value.h>
+
+static int adc_logger_thread_handle;
 
 extern "C" __EXPORT int sr_tester_main(int argc, char *argv[]);
-
-static void hwtest()
-{
-	struct actuator_controls_s actuators;
-	std::memset(&actuators, 0, sizeof(actuators));
-
-	orb_advert_t actuator_pub_fd = orb_advertise(ORB_ID(actuator_controls_0), &actuators);
-
-	int i;
-	int count = 0;
-	float rcvalue = -1.0f;
-	hrt_abstime stime;
-
-	while (count < 3) {
-		stime = hrt_absolute_time();
-
-		while (hrt_absolute_time() - stime < 1000000) {
-			for (i = 0; i < 8; i++)
-				actuators.control[i] = rcvalue;
-
-			actuators.timestamp = hrt_absolute_time();
-			orb_publish(ORB_ID(actuator_controls_0), actuator_pub_fd, &actuators);
-		}
-
-		warnx("servos set to %.1f", rcvalue);
-		rcvalue *= -1.0f;
-		count++;
-	}
-}
-
-static void vstat()
-{
-	struct vehicle_status_s status;
-	int status_handle = orb_subscribe(ORB_ID(vehicle_status));
-	orb_copy(ORB_ID(vehicle_status), status_handle, &status);
-
-	printf("condition_system_sensors_initialized: %s\n", status.condition_system_sensors_initialized ? "true" : "false");
-	printf("condition_global_position_valid: %s\n", status.condition_global_position_valid ? "true" : "false");
-	printf("condition_home_position_valid: %s\n", status.condition_home_position_valid ? "true" : "false");
-	printf("condition_local_position_valid: %s\n", status.condition_local_position_valid ? "true" : "false");
-	printf("condition_local_altitude_valid: %s\n", status.condition_local_altitude_valid ? "true" : "false");
-	printf("condition_landed: %s\n", status.condition_landed ? "true" : "false");
-
-	orb_unsubscribe(status_handle);
-}
+int adc_logger_thread_main(int argc, char *argv[]);
 
 static void usage()
 {
 	printf("Usage: sr_tester [options]\n\n");
-	printf("  -h, --help\tthis help\n");
-	printf("  -p, --vstat\tvehicle status\n");
-	printf("      --hwtest\tsimilar to the built-in hw_test example\n");
+	printf("  help\tthis help\n");
+	printf("  adc\tstart adc logger\n");
+}
+
+int adc_logger_thread_main(int argc, char *argv[])
+{
+	struct debug_key_value_s dbg;
+	struct adc_msg_s sample[12];
+	ssize_t count;
+
+	int fd = open(ADC_DEVICE_PATH, O_RDONLY);
+
+	if (fd < 0) {
+		warnx("ERROR: can't open ADC device");
+		return EXIT_FAILURE;
+	}
+
+	strcpy(dbg.key, "potmeter");
+	dbg.value = 0.0f;
+
+	orb_advert_t pub_dbg = orb_advertise(ORB_ID(debug_key_value), &dbg);
+
+	// Main loop
+	while (1) {
+		count = read(fd, sample, sizeof(sample));
+
+		if (count < 0) {
+			close(fd);
+			return EXIT_FAILURE;
+		}
+
+		int num_channels = count / sizeof(sample[0]);
+
+		// TODO: Decide which channel want to forward on mavlink
+		dbg.value = static_cast<float>(sample[0].am_data); // BEWARE uint32_t -> float?
+
+		dbg.timestamp_ms = hrt_absolute_time() / 1000.0f;
+		orb_publish(ORB_ID(debug_key_value), pub_dbg, &dbg);
+
+		usleep(10000); // Sample rate?
+	}
+
+	close(pub_dbg);
+
+	return EXIT_SUCCESS;
 }
 
 int sr_tester_main(int argc, char *argv[])
 {
-	int opt;
-	int opt_idx = 0;
-	bool continue_parse = true;
-
-	static GETOPT_LONG_OPTION_T options[] = {
-		{"help", NO_ARG, NULL, 'h'},
-		{"vstat", NO_ARG, NULL, 'v'},
-		{"hwtest", NO_ARG, NULL, 'a'},
-		{NULL, NULL, NULL, NULL}
-	};
-
-	optind = 0; // Reset optind
-
-	while (continue_parse) {
-		opt = getopt_long(argc, argv, "hv", options, &opt_idx);
-
-		if (opt == EOF) {
-			break;
-		}
-
-		switch (opt) {
-		case 'v':
-			vstat();
-			continue_parse = false;
-			break;
-
-		case 'a':
-			hwtest();
-			continue_parse = false;
-			break;
-
-		case 'h':
-		case '?':
-		default:
-			usage();
-			continue_parse = false;
-			break;
-		}
+	if (argc < 1) {
+		usage();
+	} else if (!strcmp(argv[1], "help")) {
+		usage();
+	} else if (!strcmp(argv[1], "adc")) {
+		adc_logger_thread_handle = task_spawn_cmd("adc_logger",
+					   SCHED_DEFAULT,
+					   SCHED_PRIORITY_DEFAULT,
+					   1024,
+					   static_cast<main_t>(adc_logger_thread_main),
+					   nullptr);
+	} else {
+		usage();
+		return EXIT_FAILURE;
 	}
 
 	return EXIT_SUCCESS;
